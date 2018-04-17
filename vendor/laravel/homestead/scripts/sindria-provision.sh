e6b9974ac3d82e3e7bcac9c478c3994566586131
lucapitzoi <luca.pitzoi@sindria.org>
2018-04-17 03:07:40 +0200

    fix reload tomcat service

--- vendor/laravel/homestead/scripts/sindria-provision.sh
+++ vendor/laravel/homestead/scripts/sindria-provision.sh
@@ -10,5 +10,5 @@ blockagain="
 </tomcat-users>
 "
 echo "$blockagain" > "/opt/tomcat/conf/tomcat-users.xml"
-echo "Restarting tomcat service..."
-sudo systemctl restart tomcat Again
+echo "Restarting tomcat service Again..."
+sudo systemctl restart tomcat
