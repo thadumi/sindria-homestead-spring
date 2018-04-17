7b2aa28859430225b03487b377c98927aaa4ea23
lucapitzoi <luca.pitzoi@sindria.org>
2018-04-17 03:37:52 +0200

    fixer tomcat tested ok

--- vendor/laravel/homestead/scripts/sindria-provision.sh
+++ vendor/laravel/homestead/scripts/sindria-provision.sh
@@ -2,13 +2,3 @@
 
 echo "Custom provision INSERT HERE"
 sleep 10
-
-blockagain="
-<tomcat-users>
-        <role rolename="manager-gui"/>
-        <user username="admin" password="admin" roles="manager-gui"/>
-</tomcat-users>
-"
-echo "$blockagain" > "/opt/tomcat/conf/tomcat-users.xml"
-echo "Restarting tomcat service Again..."
-sudo systemctl restart tomcat
