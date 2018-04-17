6ad9d5ff4d492b4bc6624622413e7ce2d30c3fb7
lucapitzoi <luca.pitzoi@sindria.org>
2018-04-17 02:50:32 +0200

    edit fixer-tomcat-permission

--- fixer-tomcat-permission.sh
+++ fixer-tomcat-permission.sh
@@ -4,4 +4,5 @@ echo "Insert app-name:"
 read appname
 cp /opt/tomcat/webapps/source_$appname/host-manager/META-INF/context.xml /opt/tomcat/webapps/host-manager/META-INF/context.xml
 cp /opt/tomcat/webapps/source_$appname/manager/META-INF/context.xml /opt/tomcat/webapps/manager/META-INF/context.xml
+systemctl restart tomcat
 echo "Permission has been fixed, thank you."
